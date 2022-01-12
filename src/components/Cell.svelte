cc9d66aea853551f157886b43499c619226573eb
Fortyseven <fortyseven@network47.org>
2022-01-11 22:34:16 -0500

    First grid

---
+++ src/components/Cell.svelte
@@ -0,0 +1,31 @@
+<script>
+  /*
+🟩 Large Green Square
+*/
+
+  export var color = 5;
+
+  const colors = {
+    0: "black",
+    1: "white",
+    2: "green",
+    3: "blue",
+    4: "brown",
+    5: "orange",
+    6: "purple",
+    7: "red",
+    8: "yellow",
+  };
+</script>
+
+<cell style="background-color:{colors[color]}">&nbsp;</cell>
+
+<style>
+  cell {
+    color: #333;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    border: 1px solid #222;
+  }
+</style>
